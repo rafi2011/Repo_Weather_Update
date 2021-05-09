c02a0439634c04545a95ae273a215191ffeede21
rafi2011 <rnessa2011@gmail.com>
2021-05-08 21:46:09 -0400

    Updated the repo with html, js, css, read me and instruction. md

---
+++ script.js
@@ -0,0 +1,48 @@
+var inputCity = "new york"
+var apiUrl = "http://api.openweathermap.org/data/2.5/weather?appid=d91f911bcf2c0f925fb6535547a5ddc9&q="
+var cityInputEL = doc.getElementById("weather-search") 
+var formEL = document.getElementById("weather=form")
+
+//Listen to the submit event in form
+
+formEL.addEventListener('submit', handleSubmit)
+
+function handleSubmit(event) {
+    event.preventDefault()
+    // read value from input
+    console.log(cityInputEL.value)
+
+    // make ajax call to the weather api
+
+    fetch(apiUrl + inputCity)
+    .then(processStream)
+    .then(logCityWeather)
+    .catch(function(error){
+        console.log("Thre is an error.", error)
+    })
+
+}
+
+
+
+
+
+
+function processStream (response) {
+    return response.json()
+}
+
+function logCityWeather (data) (
+    console.log(data)
+    console.log("temparature", ((9/5)*(data.main.temp-273)+32).toFixed(2),"calvin", data.main.temp)
+    console.log("humidity", data.main.humidity)
+    console.log("wind speed", data.wind.speed)
+    var temparature = ((9/5)*(data.main.temp-273)+32).toFixed(2)
+    var humidity = data.main.humidity
+    var windSpeed = data.wind.speed
+
+
+    
+    document.getElementById('weather').textContent = "temparature:" + temparature
+    
+)
